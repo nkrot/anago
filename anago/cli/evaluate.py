587063e52c786e5b08c2d4581fdd34cded05a8e4
Nikolai Krot <n.krot@drooms.com>
2019-09-12 21:33:28 +0200

    compute precision and recall in addition to f1 score

--- anago/cli/evaluate.py
+++ anago/cli/evaluate.py
@@ -2,10 +2,12 @@
 
 import os
 import sys
+
 import plac
+import seqeval.metrics
 
 from ..utils import load_data_and_labels, MODEL_COMPONENTS
-from .common import load_model_from_directory
+from .common import load_model_from_directory, NUMBER_OF_DECIMALS
 
 @plac.annotations(
     model_dir = (
@@ -19,8 +21,19 @@ def evaluate(model_dir,
              *test_file):
     """
     Evaluate given model on given dataset, computing the following metrics:
-     * f1 score (micro)
+      * f1 score
+      * precision
+      * recall\
     """
+
+    # TODO: this somewhat repeats the code in cross_validate and can/should
+    #       be dried out.
+    required_metrics = {
+        # NOTE: seqeval implementations ignore the options :(
+        'f1_score'        : { 'average' : 'micro' },
+        'precision_score' : { 'average' : 'micro' },
+        'recall_score'    : { 'average' : 'micro' }
+    }
 
     model_dir = model_dir or '.'
 
@@ -32,13 +45,24 @@ def evaluate(model_dir,
 
     model = load_model_from_directory(model_dir)
 
-    x = []; y_true = []
+    x, y_true = [], []
     for fpath in test_file:
         bulk = load_data_and_labels(fpath)
         x.extend(bulk[0])
         y_true.extend(bulk[1])
 
-    score = model.score(x, y_true)
+    y_pred = model.predict(x)
+
+    # TODO: this somewhat repeats the code in cross_validate and can/should
+    #       be dried out.
+    scores = {}
+    for metric_name,options in required_metrics.items():
+        scoring_method = getattr(seqeval.metrics, metric_name)
+        scores[metric_name] = scoring_method(y_true, y_pred, **options)
+
+    # score = model.score(x, y_true)
 
     print("Dataset size: {}".format(len(y_true)))
-    print("F1-score: {}".format(score))
+    for metric_name in required_metrics.keys():
+        print("{}: {}".format(metric_name, round(scores.get(metric_name, 0),
+                                                 NUMBER_OF_DECIMALS)))
