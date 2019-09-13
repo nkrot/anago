3e7b368f0212876f23cd389c56e7a310c1021f89
Nikolai Krot <n.krot@drooms.com>
2019-09-13 12:57:56 +0200

    move here function for printing dataset

--- anago/cli/common.py
+++ anago/cli/common.py
@@ -35,3 +35,23 @@ def load_model_from_directory(model_dir='.'):
                           model_files['preprocessor_file'])
 
     return model
+
+def print_data_and_labels(x, y1, y2=None, **kwargs):
+    """
+    Print data (x) and labels (y1 and y2 if given).
+
+    optional keyword arguments:
+    file: a file-like object (stream); defaults to sys.stdout
+    """
+
+    fd = kwargs.get('file', sys.stdout)
+
+    for sent_idx in range(len(x)):
+        if y2:
+            sent_words = zip(x[sent_idx], y1[sent_idx], y2[sent_idx])
+        else:
+            sent_words = zip(x[sent_idx], y1[sent_idx])
+
+        for word in sent_words:
+            print("\t".join(word), file=fd)
+        print(file=fd)
