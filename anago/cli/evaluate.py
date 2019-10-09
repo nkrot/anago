9b77815f4fdd2558986ed7d791339456f78a0d64
Nikolai Krot <n.krot@drooms.com>
2019-10-09 23:53:32 +0200

    show statistics per entity type both in machine- and human-readable formats

--- anago/cli/evaluate.py
+++ anago/cli/evaluate.py
@@ -1,6 +1,7 @@
 # coding: utf8
 
 import os
+import re
 import sys
 
 import plac
@@ -21,7 +22,9 @@ from . import common
         "output test dataset with predicted tags appended to corresponding lines"
         " (tsv format). This mode implies --output.",
         "flag", "a"),
-    test_file = ("path to test dataset (tsv format)", "positional")
+    test_file = (
+        "path to test dataset (tsv format)",
+        "positional")
 )
 
 def evaluate(model_dir,
@@ -32,7 +35,10 @@ def evaluate(model_dir,
     Evaluate given model on given dataset, computing the following metrics:
       * f1 score
       * precision
-      * recall\
+      * recall
+    The metrics will be computed both across all predictions as a whole and
+    for each entity type (e.g. PER, ORG, etc) individually. The latter will
+    be shown twice -- in machine-readable and human-friendly formats.\
     """
 
     model_dir = model_dir or '.'
@@ -53,12 +59,8 @@ def evaluate(model_dir,
     model = common.load_model_from_directory(model_dir)
     y_pred = model.predict(x)
 
-    # TODO: this somewhat repeats the code in cross_validate and can/should
-    #       be dried out.
-    scores = {}
-    for metric_name,options in common.REQUIRED_METRICS.items():
-        scoring_method = getattr(seqeval.metrics, metric_name)
-        scores[metric_name] = scoring_method(y_true, y_pred, **options)
+    scores           = common.collect_scores(y_true, y_pred)
+    scores_ent_types = collect_scores_per_entity_type(y_true, y_pred)
 
     # output predictions
     if append:
@@ -66,8 +68,67 @@ def evaluate(model_dir,
     elif output:
         common.print_data_and_labels(x, y_pred)
 
-    # output scores
-    print("Dataset size: {}".format(len(y_true)))
+    # output statistics
+    print("=== Statistics ===")
+
+    # overall scores
+    fmt = "{:21}: {}"
+    print(fmt.format("Dataset size", len(y_true)))
     for metric_name in common.REQUIRED_METRICS.keys():
-        print("{}: {}".format(metric_name, round(scores.get(metric_name, 0),
-                                                 common.NUMBER_OF_DECIMALS)))
+        print(fmt.format(metric_name, scores.get(metric_name, 0)))
+
+    # per-tag scores (machine-readable)
+    print("")
+    fmt = "{:16}[{}]: {}"
+    for ent_type,_scores in scores_ent_types.items():
+        for metric_name in common.REQUIRED_METRICS.keys():
+            print(fmt.format(metric_name, ent_type, _scores.get(metric_name, 0)))
+
+    # the same as the above but more human-friendly
+    print("")
+    print(seqeval.metrics.classification_report(y_true, y_pred))
+    print("=== End of Statistics ===")
+
+    pass
+
+def collect_scores_per_entity_type(y_true, y_pred):
+    """
+    Collect scores for each entity type individually.
+    Entity types are for example PER, LOC, etc as opposed to tags B-PER, I-PER,
+    B-LOC, I-LOC and so on.
+
+    Return:
+      A dictionary of the form:
+        {
+           entity_type_1 : { metric_1 : value, metric_2 : value, ... },
+           entity_type_2 : { metric_1 : value, metric_2 : value, ... }
+           ...
+        }
+    """
+    ent_types = set()
+    for sent in y_true: ent_types.update(set(sent))
+    for sent in y_pred: ent_types.update(set(sent))
+    ent_types = set([t[2:] for t in ent_types if re.match('[BIES]-', t)])
+
+    scores = {}
+    for ent_type in sorted(ent_types):
+        _y_true = keep_entity_type(y_true, ent_type, 'O')
+        _y_pred = keep_entity_type(y_pred, ent_type, 'O')
+        scores[ent_type] = common.collect_scores(_y_true, _y_pred)
+
+    return scores
+
+def keep_entity_type(predictions, ent_type, repl):
+    """
+    In given <predictions>, keep only tags that correspond to the given
+    entity type <ent_type> rewriting all other tags to <repl>.
+
+    For example, if <ent_type> is PER, then B-PER, I-PER will be kept and all
+    other tags will be rewritten to <repl>.
+    """
+    changed = [
+        tag if re.match(f'[BIES]-{ent_type}$', tag) else repl
+        for sent in predictions
+        for tag in sent
+    ]
+    return changed
